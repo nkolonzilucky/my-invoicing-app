0e7a90f1b66467231c4b339fc9739cbff3366133
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-04-01 21:19:29 +0000

    Added a form

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,11 +1,37 @@
-import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import Link from 'next/link';
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
     return (
-        <main className="pt-6 pl-6 flex flex-col text-center gap-6 h-screen mx-auto">
-            <div className="flex justify-between">
-                <h1 className="text-3xl font-bold">Create Invoices</h1>
+        <main className="pt-18 pl-6 flex flex-col gap-6 h-screen">
+            <div className="flex flex-col justify-between">
+                <h1 className="text-3xl font-bold mb-4">Create a New Invoices</h1>
+                <form className="grid gap-4 max-w-sm">
+                    <div>
+                        <Label className="block font-semibold text-sm mb-2" htmlFor="billing_name">Billing Name
+                        </Label>
+                        <Input id="billing_name" type="text" name="billing_name" />
+                    </div>
+                    <div>
+
+                        <Label className="block font-semibold text-sm mb-2" htmlFor="billing_email">Billing Email</Label>
+                        <Input id="billing_email" type="text" name="billing_email" />
+                    </div>
+                    <div>
+                        <Label className="block font-semibold text-sm mb-2" htmlFor="value">Value</Label>
+                        <Input id="value" type="text" name="value" />
+                    </div>
+                    <div>
+                        <Label className="block font-semibold text-sm mb-2" htmlFor="description">Description</Label>
+                        <Textarea id="description" name="description" />
+                    </div>
+                    <div>
+                        <Button className="w-full font-semibold">Submit</Button>
+                    </div>
+                </form>
             </div>
         </main>
     );
