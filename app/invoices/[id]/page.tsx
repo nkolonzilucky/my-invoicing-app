65be401755a6041ce404ba8b4b6d7529f3150ba5
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-04-03 22:39:27 +0000

    Added an Invoice dynamic page

---
+++ app/invoices/[id]/page.tsx
@@ -0,0 +1,57 @@
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+export default async function Invoice({ params }: { params: { id: string } }) {
+    const { id }  =  await params;
+    const [result] = await db.select().from(Invoices).where(eq(Invoices.id, parseInt(id))).limit(1);
+    console.log(`our result: ${result.email}`)
+    return (
+        <main className="max-w-5xl mx-auto my-12">
+            <div className="flex items-center mb-8 gap-4">
+                <h1 className="text-3xl font-semibold" >Invoice #{id}</h1>
+                <Badge className={cn(
+                    "rounded-full",
+                    result.status === 'open' && 'bg-green-500',
+                    result.status === 'paid' && 'bg-green-600',
+                    result.status === 'void' && 'bg-zinc-700',
+                    result.status === 'uncollectible' && 'bg-red-600',
+                )}>
+                    {result.status}
+                </Badge>
+                <p>
+
+                </p>
+            </div>
+            <p className="text-3xl mb-3">
+                {(result.value/100).toFixed(2)}
+            </p>
+            <p className="text-lg mb-8">
+                {result.description}
+            </p>
+            <h2 className="font-bold text-lg mb-4">
+                Billing Details
+            </h2>
+            <ul className="grid gap-2">
+                <li className="flex gap-4">
+                    <strong className="block w-28 flex-shrink-0 font-medium text-sm">Invoice ID</strong>
+                    <span>{result.id}</span>
+                </li>
+                <li className="flex gap-4">
+                    <strong className="block w-28 flex-shrink-0 font-medium text-sm">Invoice Date</strong>
+                    <span>{new Date(result.createTs).toLocaleDateString()}</span>
+                </li>
+                <li className="flex gap-4">
+                    <strong className="block w-28 flex-shrink-0 font-medium text-sm">Billing Name</strong>
+                    <span>{result.name}</span>
+                </li>
+                <li className="flex gap-4">
+                    <strong className="block w-28 flex-shrink-0 font-medium text-sm">Billing Email</strong>
+                    <span>{result.email}</span>
+                </li>
+            </ul>
+        </main>
+    );
+}
