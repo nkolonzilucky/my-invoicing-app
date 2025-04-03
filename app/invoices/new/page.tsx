a731ff0b0a00b32abda9fd39b22d1255d4e9fa9b
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-04-03 03:05:58 +0000

    Done with Form

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -3,31 +3,40 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import SubmitButton from "@/components/SubmitButton";
-import { Form } from 'next/form';
+import  Form  from 'next/form';
 import { createAction } from '@/app/actions';
 import { startTransition, SyntheticEvent, useState } from "react";
 
 export default function Dashboard() {
     // const result = await db.execute(sql`SELECT current_database()`);
+
     const [state, setState] = useState('ready')
     async function handleOnSubmit(event: SyntheticEvent) {
-        event.preventDefault()
-        if (state === 'pending') return;
+        
+        if (state === 'pending') {
+            console.log("State is pending")            
+            event.preventDefault()
+            return;
+        }
         setState('pending');
-        const target = event.target as HTMLFormElement;
-        startTransition(async () => {
-            const formData = new FormData(target);
-            await createAction(formData)
-            console.log("hey")
-        })
+
+
+        // const target = event.target as HTMLFormElement;
+        // startTransition(async () => {
+        //     const formData = new FormData(target);
+        //     await createAction(formData)
+        //     console.log("hey")
+        // })
 
     }
+
     return (
         <main className="pt-18 pl-6 flex flex-col gap-6 h-screen">
             <div className="flex flex-col justify-between">
                 <h1 className="text-3xl font-bold mb-4">Create a New Invoices</h1>
                 {/* {JSON.stringify(result)} */}
-                <Form onSubmit={handleOnSubmit} className="grid gap-4 max-w-sm">
+                <Form action={createAction} 
+                onSubmit={handleOnSubmit} className="grid gap-4 max-w-sm">
                     <div>
                         <Label className="block font-semibold text-sm mb-2" htmlFor="billing_name">Billing Name
                         </Label>
